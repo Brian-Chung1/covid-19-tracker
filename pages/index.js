7d2aafe16bf4bd20d5b52ab9d2e668f355dc3a47
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-05-06 12:23:03 -0700

    Added USA and US States homepage

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,123 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
-import Map from '../components/Map';
-import Footer from '../components/Footer';
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Statistics, SkeletonStatistics } from '../components/Statistics';
+import Map, { SkeletonMap } from '../components/Map';
+
+import { getStatesData } from '../hooks/useStatesData';
+import { getCountryData } from '../hooks/useCountryData';
+import {
+  getCountryVaccineData,
+  getAllStatesVaccineData,
+} from '../hooks/useVaccineData';
+
+import {
+  formatNumber,
+  convertStateDataForMapChart,
+  setCountryStatCardData,
+} from '../utils/index';
+
+import { useQuery, QueryClient } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
+
+const useStyles = makeStyles((theme) => ({}));
+
+export async function getServerSideProps() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery('USA', () => getCountryData('USA'));
+  await queryClient.prefetchQuery('US-States', getStatesData);
+  await queryClient.prefetchQuery('USAVaccine', () =>
+    getCountryVaccineData('usa', false, 1)
+  );
+  await queryClient.prefetchQuery('US-States-Vaccine', () =>
+    getAllStatesVaccineData(false, 1)
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 
 export default function Home() {
+  const classes = useStyles();
+
+  const {
+    data: countryData,
+    isLoading: isCountryLoading,
+    isError: isCountryError,
+    error: countryError,
+  } = useQuery('USA', () => getCountryData('USA'));
+
+  const {
+    data: statesData,
+    isLoading: isStatesLoading,
+    isError: isStatesError,
+    error: statesError,
+  } = useQuery('US-States', getStatesData);
+
+  const {
+    data: countryVaccineData,
+    isLoading: isCountryVaccineLoading,
+    isError: isCountryVaccineError,
+    error: countryVaccineError,
+  } = useQuery('USAVaccine', () => getCountryVaccineData('usa', false, 1));
+
+  const {
+    data: stateVaccineData,
+    isLoading: isStateVaccineLoading,
+    isError: isStateVaccineError,
+    error: stateVaccineError,
+  } = useQuery('US-States-Vaccine', () => getAllStatesVaccineData(false, 1));
+
+  if (
+    isCountryLoading ||
+    isStatesLoading ||
+    isCountryVaccineLoading ||
+    isStateVaccineLoading
+  ) {
+    return (
+      <Grid container spacing={3}>
+        {/* Statistics */}
+        <Grid item xs={12}>
+          <SkeletonStatistics />
+        </Grid>
+        {/* US Map */}
+        <Grid item xs={false} sm={12} md={12} lg={12}>
+          <SkeletonMap />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  if (
+    isCountryError ||
+    isStatesError ||
+    isCountryVaccineError ||
+    isStateVaccineError
+  ) {
+    //Error Notification;
+    console.log(countryError);
+    console.log(statesError);
+    console.log(countryVaccineError);
+    console.log(stateVaccineError);
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>COVID-19 Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>COVID-19 Tracker</h1>
-
-        <p className={styles.description}>United States Statistics</p>
-
-        {/* Stat Cards */}
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3> Total Cases </h3>
-            <p>1234</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3> Total Vaccinations </h3>
-            <p>1234</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3> Total Recovered </h3>
-            <p>1234</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3> Total Deaths </h3>
-            <p>1234</p>
-          </div>
-        </div>
-      </main>
-
-      <div className={styles.map}>
-        <Map />
-      </div>
-
-      <footer className={styles.footer}>
-        <Footer />
-      </footer>
-    </div>
+    <Grid container spacing={3}>
+      {/* Statistics */}
+      <Grid item xs={12}>
+        <Statistics
+          data={setCountryStatCardData(countryData, countryVaccineData)}
+        />
+      </Grid>
+      {/* US Map */}
+      <Grid item xs={false} sm={12}>
+        <Map data={convertStateDataForMapChart(statesData, stateVaccineData)} />
+      </Grid>
+    </Grid>
   );
 }
