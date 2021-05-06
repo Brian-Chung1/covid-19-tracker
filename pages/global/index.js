93052647130b26fe0d18e2340c786880c157ddd2
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-05-06 12:24:29 -0700

    Added global page

---
+++ pages/global/index.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+import { Statistics, SkeletonStatistics } from '../../components/Statistics';
+import Map, { SkeletonMap } from '../../components/Map';
+
+import { getGlobalData } from '../../hooks/useGlobalData';
+
+import {
+  formatNumber,
+  convertGlobalDataForGlobalMapChart,
+  setGlobalStatCardData,
+} from '../../utils/index';
+
+import { useQuery, QueryClient } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
+
+const useStyles = makeStyles((theme) => ({}));
+
+export async function getServerSideProps() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery('Global', getGlobalData);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+const Global = () => {
+  const classes = useStyles();
+
+  const { data, isLoading, isError, error } = useQuery('Global', getGlobalData);
+
+  if (isLoading) {
+    return (
+      <Grid container spacing={3}>
+        {/* Statistics */}
+        <Grid item xs={12}>
+          <SkeletonStatistics />
+        </Grid>
+        {/* US Map */}
+        <Grid item xs={false} sm={12} md={12} lg={12}>
+          <SkeletonMap />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  if (isError) {
+    //Error Notification;
+    console.log(error);
+  }
+
+  return (
+    <Grid container spacing={3}>
+      {/* Statistics */}
+      <Grid item xs={12}>
+        <Statistics data={setGlobalStatCardData(data.Global)} />
+      </Grid>
+      {/* US Map */}
+      <Grid item xs={false} sm={12}>
+        <Box border={4} borderColor="inherit">
+          <Map
+            data={convertGlobalDataForGlobalMapChart(data.Countries)}
+            isGlobalMap={true}
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Global;
