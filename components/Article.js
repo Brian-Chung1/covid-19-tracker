b7e49d4f71b40d879716a33cf0c3dc41acf7c6e3
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-05-16 21:19:18 -0700

    updated articles and added image support

--- components/Article.js
+++ components/Article.js
@@ -5,6 +5,10 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Image from 'next/image';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,13 +17,11 @@ const useStyles = makeStyles((theme) => ({
   details: {
     display: 'flex',
     flexDirection: 'column',
+    flexGrow: '2',
   },
-  content: {
-    flex: '1 0 auto',
-  },
-  cover: {
-    width: 151,
-  },
+  // cover: {
+  //   width: 151,
+  // },
 }));
 
 const SkeletonArticle = () => {
@@ -27,37 +29,67 @@ const SkeletonArticle = () => {
 };
 
 const Article = ({ data }) => {
+  const matches = useMediaQuery('(max-width:1500px)');
   const classes = useStyles();
 
-  const typeMapping = {
+  const newsTypeMapping = {
     cnn: 'CNN News',
     latimes: 'LA Times News',
     nytimes: 'NY Times News',
   };
 
+  const newsTypeImageMapping = {
+    cnn: '/CNN-logo.png',
+    latimes: '/LA-Times-logo.jpg',
+    nytimes: '/NY-Times-logo.jpg',
+  };
+
+  // all attributes: type, title, description, category, author, link, date, image,
+  //cnn attributes: title, link, type
+  //latimes attributes: title, link, description, date, image, type
+  //nytimes attributes: title, link, description, date, image, author, category, type
+
   return (
     <Card className={classes.root}>
-      {/* <CardMedia
+      <CardMedia
         className={classes.cover}
-        image={data.image ? data.image : ''}
-        title={data.title}
-      /> */}
+        style={matches ? { display: 'none' } : { display: 'flex' }}
+      >
+        <Image
+          src={data.image ? data.image : newsTypeImageMapping[data.type]}
+          alt={`${data.type} logo`}
+          width={300}
+          height={200}
+        />
+      </CardMedia>
       <div className={classes.details}>
         <CardContent className={classes.content}>
           <Typography component="h5" variant="h5">
             {data.title}
           </Typography>
+          <Divider />
           <Typography variant="subtitle1" color="textSecondary">
-            {data.author ? data.author : ''}
+            {`${data.date ? data.date : ''}${
+              data.author ? ` ${data.author}` : ''
+            }`}
           </Typography>
           <Typography variant="subtitle1" color="textSecondary">
             {data.description ? data.description : ''}
           </Typography>
           <Typography variant="subtitle1" color="textSecondary">
-            {typeMapping[data.type]}
+            {`${data.category ? `${data.category} • ` : ''}${
+              newsTypeMapping[data.type]
+            }`}
           </Typography>
         </CardContent>
       </div>
+      <CardActions className={classes.source}>
+        <Button size="small">
+          <a href={data.link} target="_blank" rel="noopener noreferrer">
+            source
+          </a>
+        </Button>
+      </CardActions>
     </Card>
   );
 };
