f4489ba55b5f836f6724024ae26092facba92048
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-05-24 14:30:07 -0700

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link rel="favicon" href="/favicon.ico" />
         </Head>
         <body>
           <script
